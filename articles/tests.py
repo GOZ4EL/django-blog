43baf37d3440281018ef78bada43b86e142d9db8
goz4el <hordemzerado@gmail.com>
2021-08-17 12:19:14 -0400

    Adding tests cases for home (Articles List View)

--- articles/tests.py
+++ articles/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from articles.models import Article
+
+class ArticleListView(TestCase):
+    """Article List View testing class"""
+    def test_view_url_exists_at_desired_location(self):
+        """The Article list view exists at '/' location."""
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        """The Article list view can be accessible by its name: 'home'."""
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        """The Article list view uses the desired template."""
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
