de29bdb1daec1eed8e99a0fe2d547f350c1c2537
goz4el <hordemzerado@gmail.com>
2021-08-17 13:02:57 -0400

    Adding test cases for Article Detail View

--- articles/.tests.py@neomake_48616_7.py
+++ articles/.tests.py@neomake_48616_7.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
 
-from django.contrib.auth.models import User
 from articles.models import Article
 
 class ArticleListView(TestCase):
@@ -24,12 +23,6 @@ class ArticleListView(TestCase):
 
 class ArticleDetailView(TestCase):
     """Article Detail View Testing Class"""
-    def test_view_default_article(self):
-        user = User.objects.create(username="authuser", password="sahfdsau1324$9'%")
-        article = Article.objects.create(
-            author=user, title="Test Art.", body="Lorem Ipsum dolor."
-        )
-        response = self.client.get(reverse('detail', args=(article.id,)))
-        self.assertContains(response, article.title)
-        self.assertContains(response, article.body)
-
+    def test_view_url_exists_at_desired_location(self):
+        article = Article.object.create()
+        response = self.client.get(revers('detail', args=()))
